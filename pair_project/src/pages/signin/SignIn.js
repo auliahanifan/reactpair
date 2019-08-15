8369f9ed3ad174fc6d14abbe8ed1cb7721cdb41c
Aulia <ahanifan@alterra.id>
2019-08-15 21:15:57 +0700

    set login is_login sudah berjalan

--- pair_project/src/pages/signin/SignIn.js
+++ pair_project/src/pages/signin/SignIn.js
@@ -2,18 +2,32 @@ import React from "react";
 import axios from "axios";
 import loginbg from "./loginbg.jpg";
 import { Link } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
+// import { store } from "../store";
+import { connect } from "unistore/react";
+import { actions } from "../../store";
 
 class SignIn extends React.Component {
   //   state = { link: "https://yesno.wtf/api" };
   constructor(props) {
     super(props);
-    this.state = {
-      question: "",
-      questionSubmit: "",
-      pic: ""
-      //   this.filterCountrySingle = this.filterCountrySingle.bind(this);
-    };
   }
+
+  postLogin = () => {
+    console.log("berhasil");
+    const self = this;
+    axios
+      .post("https://api-todofancy.herokuapp.com/api/auth")
+      .then(function(response) {
+        console.log(self.props.is_login);
+        console.log(response.data);
+        self.props.setLogin(true);
+        console.log(self.props.is_login);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
 
   render() {
     return (
@@ -28,43 +42,43 @@ class SignIn extends React.Component {
         <div className="row justify-content-center">
           <div className="col-6 text-white">
             <form style={{ padding: "200px 0px 0px 0px" }}>
-              <div class="form-group">
+              <div className="form-group">
                 <h2>Log In Form</h2>
-                <label for="exampleInputEmail1">Email address</label>
+                <label htmlFor="exampleInputEmail1">Email address</label>
                 <input
                   type="email"
-                  class="form-control"
+                  className="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                   placeholder="Enter email"
                 />
-                <small id="emailHelp" class="form-text text-muted">
+                <small id="emailHelp" className="form-text text-muted">
                   We'll never share your email with anyone else.
                 </small>
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input
                   type="password"
-                  class="form-control"
+                  className="form-control"
                   id="exampleInputPassword1"
                   placeholder="Password"
                 />
               </div>
-              <div class="form-group form-check">
+              <div className="form-group form-check">
                 <input
                   type="checkbox"
-                  class="form-check-input"
+                  className="form-check-input"
                   id="exampleCheck1"
                 />
-                <label class="form-check-label" for="exampleCheck1">
+                <label className="form-check-label" for="exampleCheck1">
                   Check me out
                 </label>
               </div>
               <button
                 type="submit"
-                class="btn btn-primary"
-                onClick={() => this.postLogin()}
+                className="btn btn-primary"
+                onClick={this.postLogin()}
               >
                 Submit
               </button>
@@ -76,4 +90,7 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+export default connect(
+  "email, full_name, is_login",
+  actions
+)(withRouter(SignIn));
