f0bdf824117d8c3caa6c399998f8e5619e1f34cb
adesupraptolaia <ades@alterra.id>
2019-08-16 08:32:44 +0700

    is_login success

--- pair_project/src/pages/catquo/fiturCat.js
+++ pair_project/src/pages/catquo/fiturCat.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./main.css";
+import { connect } from "unistore/react";
+import { actions } from "../../store";
+import { Redirect } from "react-router-dom";
 
 import Gambar from "../../component/gambarCat";
 import Quotes from "../../component/quotesCat";
@@ -10,30 +13,37 @@ const divStyle = {
 
 class Cats extends React.Component {
   render() {
-    return (
-      <div className="backCol">
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="container">
-          <div className="row text-center justify-content-center">
-            <div className="col-md-7 text-center">
-              <div class="card card-body" style={divStyle}>
-                <Gambar />
-                <div class="card-body">
-                  <h5 class="card-title">Quotes by Cat</h5>
-                  <p class="card-text">
-                    <Quotes />
-                  </p>
+    if (this.props.is_login === false) {
+      return <Redirect to={{ pathname: "/" }} />;
+    } else {
+      return (
+        <div className="backCol">
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="container">
+            <div className="row text-center justify-content-center">
+              <div className="col-md-7 text-center">
+                <div class="card card-body" style={divStyle}>
+                  <Gambar />
+                  <div class="card-body">
+                    <h5 class="card-title">Quotes by Cat</h5>
+                    <p class="card-text">
+                      <Quotes />
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
-export default Cats;
+export default connect(
+  "email, full_name, is_login, pic, question",
+  actions
+)(Cats);
